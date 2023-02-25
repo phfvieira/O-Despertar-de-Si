4a85e530062c55cd42cb3936148396673da5affa
Pablo <phf-vieira@hotmail.com>
2023-02-25 15:30:33 -0300

    second commit

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,10 @@
 jQuery(function ($) {
+    console.log("auterou");
     $(".wrapper_items").slick({
         infinite: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
+        
     });
 });
